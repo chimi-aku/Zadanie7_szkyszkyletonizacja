e68e22cc82d1adebdd9b8e369752337787971666
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2022-05-10 21:38:32 +0200

    Added Binarizations

--- src/main/java/com/example/zadanie7_szkyszkyletonizacja/Binarization.java
+++ src/main/java/com/example/zadanie7_szkyszkyletonizacja/Binarization.java
@@ -1,16 +1,23 @@
 package com.example.zadanie7_szkyszkyletonizacja;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.IOException;
-
-import static com.example.zadanie7_szkyszkyletonizacja.FileHandler.convertToFxImage;
 
 public class Binarization {
 
-    public void Niblack (int window, double k1,) throws IOException {
-        BufferedImage imageO = ImageIO.read(selectedFile);
-        BufferedImage img = ImageIO.read(selectedFile);
+    private static int width;
+    private static int height;
+    public final int pixel_ob = 0xffffffff; //object pixel
+    public final int pixel_bg = 0xff000000; //background pixel
+
+    public static BufferedImage NiblackBinarization (BufferedImage imgSrc, int window, double k ) throws IOException {
+
+        BufferedImage img = deepCopy(imgSrc);
+
+        width = img.getWidth();
+        height = img.getHeight();
 
         int iB, iG, iR, rgb, sum, num;
         double area, mean, standardDeviation, average;
@@ -26,22 +33,24 @@ public class Binarization {
                 for(int ji = -window; ji < window; ji++){
                     for(int jj = -window; jj < window; jj++){
                         if((column + ji >= 0 && column + ji < width)&&(row + jj >= 0 && row + jj < height)){
-                            rgb = imageO.getRGB(column + ji, row + jj);
+                            rgb = img.getRGB(column + ji, row + jj);
                             iR += rgb & 0xff0000 >> 16;
                             iG += rgb & 0x00ff00 >> 8;
                             iB += rgb & 0xff;
                         }
                     }
                 }
+
                 sum = (iR + iG + iB) / 3;
                 area = (window)*(window)*4;
                 standardDeviation = 0.0;
                 mean = sum/area;
+
                 for(int ji = -window; ji < window; ji++){
                     for(int jj = -window; jj < window; jj++){
                         if(column + ji >= 0 && column + ji < width){
                             if(row + jj >= 0 && row + jj < height){
-                                rgb = imageO.getRGB(column + ji, row + jj);
+                                rgb = img.getRGB(column + ji, row + jj);
                                 iR = rgb & 0xff0000 >> 16;
                                 iG = rgb & 0x00ff00 >> 8;
                                 iB = rgb & 0xff;
@@ -59,7 +68,7 @@ public class Binarization {
                 pixelA = (pixelR + pixelG + pixelB) / 3;
                 average = sum / area;
 
-                NiBlack = average + k1 * standardDeviation;
+                NiBlack = average + k * standardDeviation;
 
                 if(pixelA > NiBlack)
                     img.setRGB(column, row, 0xffffffff);
@@ -68,10 +77,194 @@ public class Binarization {
             }
         }
 
+        return img;
+    }
+
+    public static BufferedImage PhansalkarBinarization(BufferedImage imgBuff, int window, double k)
+    {
+        //Window size (for each side)
+
+        width = imgBuff.getWidth();
+        height = imgBuff.getHeight();
+        double R = 0.0;
+
+        BufferedImage img = deepCopy(imgBuff);
+
+        //Scanning the entire image
+        for (int column = 0; column < width; column++)
+        {
+            for (int row = 0; row < height; row++)
+            {
+                int iAverage = 0;
+                int iRed=0, iGreen=0, iBlue=0;
+
+                //Sweeping the window to get the average
+                for(int ji = -window ; ji < window ; ji++)
+                {
+                    for(int jj = -window ; jj < window ; jj++)
+                    {
+                        //Accumulating the values and then taking the average - do if serves to protect the window from not walking out of the image
+                        if(column+ji >= 0 && column+ji < width)
+                            if(row+jj >= 0 && row+jj < height)
+                            {
+                                iRed += imgBuff.getRGB(column+ji, row+jj) & 0xff0000 >> 16;
+                                iGreen += imgBuff.getRGB(column+ji, row+jj) & 0x00ff00 >> 8;
+                                iBlue += imgBuff.getRGB(column+ji, row+jj) & 0xff;
+                                //iAverage += imgBuff.getRGB(column+ji, row+jj) & 0x00ff0000 >> 16;
+                            }
+                    }
+                }
+                iAverage = (iRed + iGreen + iBlue ) / 3;
+
+                double area = (window*2) * (window*2);
+                double standardDeviation = 0.0;
+                double mean = iAverage/area;
+                int num;
 
 
-        niblackImage.setImage(convertToFxImage(img));
-        niblackImage.setFitHeight(150);
-        niblackImage.setFitWidth(200);
+                for(int ji = -window ; ji < window ; ji++)
+                {
+                    for(int jj = -window ; jj < window ; jj++)
+                    {
+                        //Accumulating the values and then taking the average - do if serves to protect the window from not walking out of the image
+                        if(column+ji >= 0 && column+ji < width)
+                            if(row+jj >= 0 && row+jj < height)
+                            {
+                                iRed = imgBuff.getRGB(column+ji, row+jj) & 0xff0000 >> 16;
+                                iGreen = imgBuff.getRGB(column+ji, row+jj) & 0x00ff00 >> 8;
+                                iBlue = imgBuff.getRGB(column+ji, row+jj) & 0xff;
+                                num = (iRed + iGreen + iBlue) / 3;
+
+                                //R = Math.max(R, Math.pow(num - mean, 2));
+                                standardDeviation += Math.pow(num - mean, 2);
+
+                            }
+                    }
+                }
+
+                double SD = Math.sqrt(standardDeviation/area);
+                int pixelRed = img.getRGB(column, row) & 0xff0000 >> 16;
+                int pixelGreen = img.getRGB(column, row) & 0x00ff00 >> 8;
+                int pixelBlue = img.getRGB(column, row) & 0xff;
+                int pixelAverage = (pixelRed + pixelGreen + pixelBlue) /3;
+                double average = iAverage / area;
+                R = Math.max(R, standardDeviation);
+                //***********************************//
+
+                //* constanst to implements fielsds *//
+                double p = 3;
+                double q = 10;
+
+                //Phansalkar Pattern
+                //double Phansalkar = average * (1 + k*(SD / R - 1));
+                double Phansalkar = average *  (1 + p * Math.exp(-q * average) + (k * (SD / R - 1) ));
+
+
+                //is higher than average
+                //int pixel = img.getRGB(column, row) & 0x00ff0000 >> 16;
+                if (pixelAverage > Phansalkar)
+                    img.setRGB(column, row, 0x00FFFFFF );
+                else
+                    img.setRGB(column, row, 0x00000000 );
+            }
+        }
+
+        return img;
+    }
+
+    public static BufferedImage SauvolaBinarization(BufferedImage imgBuff, int window, double k)
+    {
+        //Window size (for each side)
+
+        width = imgBuff.getWidth();
+        height = imgBuff.getHeight();
+        double R = 0.0;
+
+        BufferedImage img = deepCopy(imgBuff);
+
+        //Scanning the entire image
+        for (int column = 0; column < width; column++)
+        {
+            for (int row = 0; row < height; row++)
+            {
+                int iAverage = 0;
+                int iRed=0, iGreen=0, iBlue=0;
+
+                //Sweeping the window to get the average
+                for(int ji = -window ; ji < window ; ji++)
+                {
+                    for(int jj = -window ; jj < window ; jj++)
+                    {
+                        //Accumulating the values and then taking the average - do if serves to protect the window from not walking out of the image
+                        if(column+ji >= 0 && column+ji < width)
+                            if(row+jj >= 0 && row+jj < height)
+                            {
+                                iRed += imgBuff.getRGB(column+ji, row+jj) & 0xff0000 >> 16;
+                                iGreen += imgBuff.getRGB(column+ji, row+jj) & 0x00ff00 >> 8;
+                                iBlue += imgBuff.getRGB(column+ji, row+jj) & 0xff;
+                                //iAverage += imgBuff.getRGB(column+ji, row+jj) & 0x00ff0000 >> 16;
+                            }
+                    }
+                }
+                iAverage = (iRed + iGreen + iBlue ) / 3;
+
+                double area = (window*2) * (window*2);
+                double standardDeviation = 0.0;
+                double mean = iAverage/area;
+                int num;
+
+
+                for(int ji = -window ; ji < window ; ji++)
+                {
+                    for(int jj = -window ; jj < window ; jj++)
+                    {
+                        //Accumulating the values and then taking the average - do if serves to protect the window from not walking out of the image
+                        if(column+ji >= 0 && column+ji < width)
+                            if(row+jj >= 0 && row+jj < height)
+                            {
+                                iRed = imgBuff.getRGB(column+ji, row+jj) & 0xff0000 >> 16;
+                                iGreen = imgBuff.getRGB(column+ji, row+jj) & 0x00ff00 >> 8;
+                                iBlue = imgBuff.getRGB(column+ji, row+jj) & 0xff;
+                                num = (iRed + iGreen + iBlue) / 3;
+
+                                //R = Math.max(R, Math.pow(num - mean, 2));
+                                standardDeviation += Math.pow(num - mean, 2);
+
+                            }
+                    }
+                }
+
+                double SD = Math.sqrt(standardDeviation/area);
+                int pixelRed = img.getRGB(column, row) & 0xff0000 >> 16;
+                int pixelGreen = img.getRGB(column, row) & 0x00ff00 >> 8;
+                int pixelBlue = img.getRGB(column, row) & 0xff;
+                int pixelAverage = (pixelRed + pixelGreen + pixelBlue) /3;
+                double average = iAverage / area;
+                R = Math.max(R, standardDeviation);
+                //***********************************//
+
+                //Sauvola Pattern
+                double Sauvola = average * (1 + k*(SD / R - 1));
+
+
+                //is higher than average
+                //int pixel = img.getRGB(column, row) & 0x00ff0000 >> 16;
+                if (pixelAverage > Sauvola)
+                    img.setRGB(column, row, 0x00FFFFFF );
+                else
+                    img.setRGB(column, row, 0x00000000 );
+            }
+        }
+
+        return img;
+    }
+
+    static BufferedImage deepCopy(BufferedImage bi)
+    {
+        ColorModel cm = bi.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = bi.copyData(null);
+
+        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
     }
 }
--- src/main/java/com/example/zadanie7_szkyszkyletonizacja/HelloController.java
+++ src/main/java/com/example/zadanie7_szkyszkyletonizacja/HelloController.java
@@ -2,11 +2,11 @@ package com.example.zadanie7_szkyszkyletonizacja;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 
 public class HelloController {
@@ -22,12 +22,18 @@ public class HelloController {
     @FXML protected void onLoadImageBtnClick()
     {
         Stage thisStage = (Stage) loadImageBtn.getScene().getWindow();
-        originalImage = com.example.zadanie7_szkyszkyletonizacja.FileHandler.LoadImage(thisStage);
-        originalImageView.setImage(com.example.zadanie7_szkyszkyletonizacja.FileHandler.convertToFxImage(originalImage));
+        originalImage = FileHandler.LoadImage(thisStage);
+        originalImageView.setImage(FileHandler.convertToFxImage(originalImage));
 
     }
 
-    @FXML protected void onk3mImageBtnClick() {
+    @FXML protected void onk3mImageBtnClick() throws IOException {
 
+        // Binarization Testing
+        //BufferedImage binarizedImg = Binarization.NiblackBinarization(originalImage, 30, -.5);
+        BufferedImage binarizedImg = Binarization.PhansalkarBinarization(originalImage, 30, 0.3);
+        BufferedImage sauvolaBinarizedImg = Binarization.SauvolaBinarization(originalImage, 5, 0.5);
+
+        skeletonImageView.setImage(FileHandler.convertToFxImage(sauvolaBinarizedImg));
     }
 }
