30ef50cffd4eeb1b708673a6cf6653b9f0e49909
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2022-05-10 19:36:17 +0200

    Simple menu

--- src/main/java/com/example/zadanie7_szkyszkyletonizacja/HelloController.java
+++ src/main/java/com/example/zadanie7_szkyszkyletonizacja/HelloController.java
@@ -11,24 +11,24 @@ import java.awt.image.BufferedImage;
 
 public class HelloController {
 
+    @FXML private Button loadImageBtn;
+    @FXML private Button k3mImageBtn;
 
     private BufferedImage originalImage;
-    @FXML private Button loadImageBtn;
+
     @FXML private ImageView originalImageView;
     @FXML private ImageView skeletonImageView;
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML protected void onLoadImageBtnClick()
     {
         Stage thisStage = (Stage) loadImageBtn.getScene().getWindow();
         originalImage = com.example.zadanie7_szkyszkyletonizacja.FileHandler.LoadImage(thisStage);
         originalImageView.setImage(com.example.zadanie7_szkyszkyletonizacja.FileHandler.convertToFxImage(originalImage));
+
+    }
+
+    @FXML protected void onk3mImageBtnClick() {
 
     }
 }
